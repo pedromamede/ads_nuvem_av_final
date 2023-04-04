158f4c5e5988b31271e985ed4d4bf3778edc712c
carlosalves91 <>
2023-04-04 21:28:21 +0000

    Adicionando alterações no index + criação arquivo de cadastro

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const baseRoute = require('./base');
 const carroRoute = require('./carro');
 const endereco_lojaRoute = require('./endereco_loja');
+const cadastro_clienteRoute = require('./cadastro_cliente');
 const path = require('path');
 
 const app = express();
@@ -13,7 +14,7 @@ app.use(express.static('public'));
 app.use('/', baseRoute);
 app.use('/carros', carroRoute);
 app.use('/endereco_lojas', endereco_lojaRoute);
-
+app.use('/cadastro_clientes', cadastro_clienteRoute);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
